dbfb987f3b82ca075390b11bce6dd695b72d75c2
cah07r <cah07r@ecs.soton.ac.uk>
2016-06-08 11:51:00 +0100

    SSL set up

--- server.js
+++ server.js
@@ -12,6 +12,16 @@ var Logger = require('./utilities/Logger.js');
 var Express = require('express');
 var Mongoose = require('mongoose');
 var Routes = require('./routes.js');
+var https = require('https');
+var fs = require('fs');
+
+// Set up SSL ------------------------------------------------------
+var key = fs.readFileSync('../storyplaces-key.pem');
+var cert = fs.readFileSync('../storyplaces-cert.pem')
+var https_options = {
+    key: key,
+    cert: cert
+};
 
 // Build our express app ------------------------------------------------------
 var App = Express();
@@ -24,5 +34,7 @@ App.use(settings.api.url,       Routes);
 App.use(settings.client.url,    Express.static(settings.client.source_path));
 
 // Start the server -----------------------------------------------------------
-App.listen(port);
+//App.listen(port);
+https.createServer(https_options, App).listen(port);
+
 Logger.log('Serving on port ' + port);
