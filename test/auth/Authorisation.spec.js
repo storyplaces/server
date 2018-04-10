886996ffcec09a14aa2da38b7aa5294809308347
Kevin <kevin@algn.uk>
2018-04-10 14:30:55 +0100

    Fixes for authentication tests

--- test/auth/Authorisation.spec.js
+++ test/auth/Authorisation.spec.js
@@ -1,22 +1,22 @@
-//During the test the env variable is set to test
+//During the test the env letiable is set to test
 process.env.NODE_ENV = 'test';
 
 //Require the dev-dependencies
-var chai = require('chai');
-var should = chai.should();
-var expect = chai.expect;
+let chai = require('chai');
+let should = chai.should();
+let expect = chai.expect;
 
-var authorisationFunctions = require('../../auth/Authorisation');
+let authorisationFunctions = require('../../auth/Authorisation');
 
 require('../utils/testing-utils');
-var factories = require('../utils/testing-factories');
+let factories = require('../utils/testing-factories');
 
 describe("Authorisation", function () {
 
     describe("convertRolesToPrivileges", function () {
         context("when given a role that exists", function () {
             it("returns an array of privileges", function () {
-                var result = authorisationFunctions.convertRolesToPrivileges(["author"]);
+                let result = authorisationFunctions.convertRolesToPrivileges(["author"]);
 
                 Array.isArray(result).should.equal(true);
                 result.length.should.not.equal(0);
@@ -26,35 +26,42 @@ describe("Authorisation", function () {
 
         context("when given a role that does not exists", function () {
             it("returns an empty array", function () {
-                var result = authorisationFunctions.convertRolesToPrivileges(["dogs body"]);
+                let result = authorisationFunctions.convertRolesToPrivileges(["dogs body"]);
 
                 Array.isArray(result).should.equal(true);
                 result.length.should.equal(0);
             })
         });
 
-        context("when given two roles with overlapping privileges", function () {
-            it("returns an array of unique privileges")
+        context("when given two roles", function () {
+            it("returns an array of privileges based on both roles", function () {
+                let result = authorisationFunctions.convertRolesToPrivileges(["author", "admin"]);
+
+                Array.isArray(result).should.equal(true);
+                result.length.should.not.equal(0);
+                result.contains("createStory").should.equal(true);
+                result.contains("deleteReadingStory").should.equal(true);
+            })
         });
     });
 
     describe("hasPrivileges", function () {
-        var userPrivileges = ["edit", "save", "delete"];
+        let userPrivileges = ["edit", "save", "delete"];
 
         context("when the match is set to 'all'", function () {
-            var match = "all";
+            let match = "all";
 
             context("a single item array is passed as the required privilege", function () {
                 context("the passed required privilege is in the user privileges", function () {
                     it("returns true", function () {
-                        var result = authorisationFunctions.hasPrivileges(["edit"], userPrivileges, match);
+                        let result = authorisationFunctions.hasPrivileges(["edit"], userPrivileges, match);
                         result.should.equal(true);
                     });
                 });
 
                 context("the passed required privilege is not in the user privileges", function () {
                     it("returns false", function () {
-                        var result = authorisationFunctions.hasPrivileges(["create"], userPrivileges, match);
+                        let result = authorisationFunctions.hasPrivileges(["create"], userPrivileges, match);
                         result.should.equal(false);
                     });
                 });
@@ -63,21 +70,21 @@ describe("Authorisation", function () {
             context("a multiple item array is passed as the required privilege", function () {
                 context("some of the the passed required privilege are in the user privileges", function () {
                     it("returns false", function () {
-                        var result = authorisationFunctions.hasPrivileges(["edit", "create"], userPrivileges, match);
+                        let result = authorisationFunctions.hasPrivileges(["edit", "create"], userPrivileges, match);
                         result.should.equal(false);
                     });
                 });
 
                 context("all of the the passed required privilege are in the user privileges", function () {
                     it("returns true", function () {
-                        var result = authorisationFunctions.hasPrivileges(["edit", "save"], userPrivileges, match);
+                        let result = authorisationFunctions.hasPrivileges(["edit", "save"], userPrivileges, match);
                         result.should.equal(true);
                     });
                 });
 
                 context("none of the the passed required privilege are in the user privileges", function () {
                     it("returns false", function () {
-                        var result = authorisationFunctions.hasPrivileges(["create", "upload"], userPrivileges, match);
+                        let result = authorisationFunctions.hasPrivileges(["create", "upload"], userPrivileges, match);
                         result.should.equal(false);
                     });
                 });
@@ -85,19 +92,19 @@ describe("Authorisation", function () {
         });
 
         context("when the match is set to 'any'", function () {
-            var match = "any";
+            let match = "any";
 
             context("a single item array is passed as the required privilege", function () {
                 context("the passed required privilege is in the user privileges", function () {
                     it("returns true", function () {
-                        var result = authorisationFunctions.hasPrivileges(["edit"], userPrivileges, match);
+                        let result = authorisationFunctions.hasPrivileges(["edit"], userPrivileges, match);
                         result.should.equal(true);
                     });
                 });
 
                 context("the passed required privilege is not in the user privileges", function () {
                     it("returns false", function () {
-                        var result = authorisationFunctions.hasPrivileges(["create"], userPrivileges, match);
+                        let result = authorisationFunctions.hasPrivileges(["create"], userPrivileges, match);
                         result.should.equal(false);
                     });
                 });
@@ -106,21 +113,21 @@ describe("Authorisation", function () {
             context("a multiple item array is passed as the required privilege", function () {
                 context("some of the the passed required privilege are in the user privileges", function () {
                     it("returns true", function () {
-                        var result = authorisationFunctions.hasPrivileges(["edit", "create"], userPrivileges, match);
+                        let result = authorisationFunctions.hasPrivileges(["edit", "create"], userPrivileges, match);
                         result.should.equal(true);
                     });
                 });
 
                 context("all of the the passed required privilege are in the user privileges", function () {
                     it("returns true", function () {
-                        var result = authorisationFunctions.hasPrivileges(["edit", "save"], userPrivileges, match);
+                        let result = authorisationFunctions.hasPrivileges(["edit", "save"], userPrivileges, match);
                         result.should.equal(true);
                     });
                 });
 
                 context("none of the the passed required privilege are in the user privileges", function () {
                     it("returns false", function () {
-                        var result = authorisationFunctions.hasPrivileges(["create", "upload"], userPrivileges, match);
+                        let result = authorisationFunctions.hasPrivileges(["create", "upload"], userPrivileges, match);
                         result.should.equal(false);
                     });
                 });
@@ -132,14 +139,14 @@ describe("Authorisation", function () {
             context("a single item array is passed as the required privilege", function () {
                 context("the passed required privilege is in the user privileges", function () {
                     it("returns true", function () {
-                        var result = authorisationFunctions.hasPrivileges(["edit"], userPrivileges);
+                        let result = authorisationFunctions.hasPrivileges(["edit"], userPrivileges);
                         result.should.equal(true);
                     });
                 });
 
                 context("the passed required privilege is not in the user privileges", function () {
                     it("returns false", function () {
-                        var result = authorisationFunctions.hasPrivileges(["create"], userPrivileges);
+                        let result = authorisationFunctions.hasPrivileges(["create"], userPrivileges);
                         result.should.equal(false);
                     });
                 });
@@ -148,21 +155,21 @@ describe("Authorisation", function () {
             context("a multiple item array is passed as the required privilege", function () {
                 context("some of the the passed required privilege are in the user privileges", function () {
                     it("returns true", function () {
-                        var result = authorisationFunctions.hasPrivileges(["edit", "create"], userPrivileges);
+                        let result = authorisationFunctions.hasPrivileges(["edit", "create"], userPrivileges);
                         result.should.equal(true);
                     });
                 });
 
                 context("all of the the passed required privilege are in the user privileges", function () {
                     it("returns true", function () {
-                        var result = authorisationFunctions.hasPrivileges(["edit", "save"], userPrivileges);
+                        let result = authorisationFunctions.hasPrivileges(["edit", "save"], userPrivileges);
                         result.should.equal(true);
                     });
                 });
 
                 context("none of the the passed required privilege are in the user privileges", function () {
                     it("returns false", function () {
-                        var result = authorisationFunctions.hasPrivileges(["create", "upload"], userPrivileges);
+                        let result = authorisationFunctions.hasPrivileges(["create", "upload"], userPrivileges);
                         result.should.equal(false);
                     });
                 });
