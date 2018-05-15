86dc352f1ba154fb8d3d84f73e18021357872bc0
Kevin <kevin@algn.uk>
2018-05-15 12:07:59 +0100

    Allow links to any page

--- conversion/Markdown.js
+++ conversion/Markdown.js
@@ -13,14 +13,6 @@ renderer.image = function (href, title, text) {
     return "";
 };
 
-renderer.link = function(href, title, text) {
-    if (href.startsWith('https://www.isurvey.soton.ac.uk')) {
-        return `<a href="${href}" title="${title}">${text}</a>`
-    }
-
-    return "";
-};
-
 marked.setOptions({
     renderer: renderer,
     gfm: true,
